c25894e0588ddbd0403a4e2fe3b80d75dafa058d
Shiva9966 <167856895+Shiva9966@users.noreply.github.com>
2026-01-20 14:35:29 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
